8e1eec1d5859b7ccc91b28b3517553c4ad02667a
Evgeniy Datskij <evgenij.datskij@mail.ru>
2023-02-24 23:48:42 +0500

    start work on form registration

--- src/pages/Login/index.jsx
+++ src/pages/Login/index.jsx
@@ -3,26 +3,54 @@ import Typography from "@mui/material/Typography";
 import TextField from "@mui/material/TextField";
 import Paper from "@mui/material/Paper";
 import Button from "@mui/material/Button";
-
+import { useForm } from "react-hook-form";
 import styles from "./Login.module.scss";
 
 export const Login = () => {
+  const {
+    register,
+    handleSubmit,
+    setError,
+    formState: { errors, isValid },
+  } = useForm({
+    defaultValues: {
+      email: "",
+      password: "",
+    },
+    mode: "onChange",
+  });
+
+  // функция которая будет выполняться лишь тогда когда реакт хукс понял, что валидация прошла успешно
+  const onSubmit = (values) => {
+    console.log(values);
+  };
   return (
     <Paper classes={{ root: styles.root }}>
       <Typography classes={{ root: styles.title }} variant="h5">
         Вход в аккаунт
       </Typography>
-      <TextField
-        className={styles.field}
-        label="E-Mail"
-        error
-        helperText="Неверно указана почта"
-        fullWidth
-      />
-      <TextField className={styles.field} label="Пароль" fullWidth />
-      <Button size="large" variant="contained" fullWidth>
-        Войти
-      </Button>
+      <form onSubmit={handleSubmit(onSubmit)}>
+        <TextField
+          className={styles.field}
+          label="E-Mail"
+          error={Boolean(errors.email?.message)}
+          // если эта хрень ^ присутствует, то форма красным подсвечивает
+          helperText={errors.email?.message}
+          {...register("email", { required: "Укажите почту" })}
+          fullWidth
+        />
+        <TextField
+          className={styles.field}
+          label="Пароль"
+          error={Boolean(errors.password?.message)}
+          helperText={errors.password?.message}
+          {...register("password", { required: "Укажите пароль" })}
+          fullWidth
+        />
+        <Button size="large" variant="contained" fullWidth>
+          Войти
+        </Button>
+      </form>
     </Paper>
   );
 };
--- src/redux/store.js
+++ src/redux/store.js
@@ -1,9 +1,11 @@
 import { configureStore } from "@reduxjs/toolkit";
 import { postsReducer } from "./slices/posts.js";
+import { authReduser } from "./slices/auth.js";
 
 const store = configureStore({
   reducer: {
     posts: postsReducer,
+    auth: authReduser,
   },
 });
 
