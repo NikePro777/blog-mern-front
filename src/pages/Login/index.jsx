f297a4a6737bcc6ebafecd8f38bbbeb06f2e7fb6
Evgeniy Datskij <evgenij.datskij@mail.ru>
2023-02-25 17:46:17 +0500

    auth and disauth

--- src/pages/Login/index.jsx
+++ src/pages/Login/index.jsx
@@ -1,12 +1,17 @@
 import React from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { Navigate } from "react-router-dom";
 import Typography from "@mui/material/Typography";
 import TextField from "@mui/material/TextField";
 import Paper from "@mui/material/Paper";
 import Button from "@mui/material/Button";
 import { useForm } from "react-hook-form";
 import styles from "./Login.module.scss";
+import { fetchAuth, selectIsAuth } from "../../redux/slices/auth";
 
 export const Login = () => {
+  const isAuth = useSelector(selectIsAuth);
+  const dispatch = useDispatch();
   const {
     register,
     handleSubmit,
@@ -14,16 +19,19 @@ export const Login = () => {
     formState: { errors, isValid },
   } = useForm({
     defaultValues: {
-      email: "",
-      password: "",
+      email: "e011@yandex.ru",
+      password: "hjhjhhjjg",
     },
     mode: "onChange",
   });
 
   // функция которая будет выполняться лишь тогда когда реакт хукс понял, что валидация прошла успешно
   const onSubmit = (values) => {
-    console.log(values);
+    dispatch(fetchAuth(values));
   };
+  if (isAuth) {
+    return <Navigate to="/" />;
+  }
   return (
     <Paper classes={{ root: styles.root }}>
       <Typography classes={{ root: styles.title }} variant="h5">
@@ -36,6 +44,7 @@ export const Login = () => {
           error={Boolean(errors.email?.message)}
           // если эта хрень ^ присутствует, то форма красным подсвечивает
           helperText={errors.email?.message}
+          type="email"
           {...register("email", { required: "Укажите почту" })}
           fullWidth
         />
@@ -43,11 +52,12 @@ export const Login = () => {
           className={styles.field}
           label="Пароль"
           error={Boolean(errors.password?.message)}
+          type="password"
           helperText={errors.password?.message}
           {...register("password", { required: "Укажите пароль" })}
           fullWidth
         />
-        <Button size="large" variant="contained" fullWidth>
+        <Button type="submit" size="large" variant="contained" fullWidth>
           Войти
         </Button>
       </form>
